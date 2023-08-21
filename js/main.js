a546c756d229a8fa697c6f4274cc3270004220d0
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-21 17:49:42 +0200

    added a function to generate cells

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
+// get start game button
 const startButton = document.getElementById("button-start");
-
+// get game grid
 const gameGrid = document.getElementById("game-grid");
+
+const grid = 10;
+
+// start button functionality
 
 startButton.addEventListener("click", function () {
   gameGrid.innerHTML = "";
   gridGenerator();
 });
 
+// function for generating grid
+
 function gridGenerator() {
-  for (let index = 1; index <= 100; index++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.innerHTML = index;
-
-    cell.addEventListener("click", function () {
-      this.classList.add("azure");
-      console.log(index);
-    });
-
-    gameGrid.append(cell);
+  for (let index = 1; index <= grid * grid; index++) {
+    gameGrid.append(cellGenerator(index));
   }
+}
+
+// function for generating cells
+
+function cellGenerator(number) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.innerHTML = number;
+
+  cell.addEventListener("click", function () {
+    this.classList.add("azure");
+    console.log(number);
+  });
+
+  return cell;
 }
